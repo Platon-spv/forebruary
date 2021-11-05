c2cfed1954423f15e9b8fc8865079d429331ba10
Platon <platon.webdev@gmail.com>
2021-11-05 04:21:18 +0300

    Add touch events, small code review
    Add touch events, small code review

--- forebruary.js
+++ forebruary.js
@@ -3,7 +3,7 @@ var from = 0
 var measure = 68 //75
 var targetX = measure * 4
 var x = targetX
-var $me = $ ('.cal-frame')
+var $frame = $ ('.cal-frame')
 
 
 var grav = function (what) {
@@ -14,39 +14,48 @@ var sel = function (who, num) {
   $ ('.monthsel').not (who).removeClass ('selected')
   $ (who).addClass ('selected')
   targetX = (measure * 4) + num * measure
-  $me.animate ({
+  $frame.animate ({
     'left': targetX + 'px',
     'top': '0px'
   }, 500)
   return false
 }
 
-$me.mousedown (function (e) { //!!!!!
-  $me.stop ()
+
+
+$frame.on('touchstart mousedown',function (e) { //!!!!!
+  $frame.stop ()
   moving = true
   x = targetX
-  from = x - e.clientX
+  from = x - (e.clientX || e.touches[0].clientX)
   e.preventDefault ()
+  console.log("touchstart mousedown")
 })
 
-$me.mouseup (function (e) { //!!!!!!
+$frame.on('touchend mouseup',function (e) { //!!!!!!
   moving = false
   targetX = x - (x - (measure / 2)) % measure + (measure / 2);
-  $me.animate ({
+  $frame.animate ({
     'left': targetX + 'px',
     'top': '0px'
   }, 100)
+  console.log("touchend mouseup")
+  
 })
 
-$me.mousemove (function (e) {//!!!!!!!
+$frame.on('touchmove mousemove', function (e) {//!!!!!!!
   if (moving) {
-    x = from + e.clientX
+
+    x = from + (e.clientX || e.touches[0].clientX)
     if (x < measure) x = measure
     if (x > measure * 7) x = measure * 7
     y = 0; //- Math.abs (grav (x))
-    $me.css ('left', x + 'px')
-    $me.css ('top', y + 'px')
+    $frame.css ('left', x + 'px')
+    $frame.css ('top', y + 'px')
     $ ('.monthsel').removeClass ('selected')
+  console.log("touchmove mousemove")
+  console.log("moving", moving)
+
   }
   e.preventDefault ()
   return false
@@ -55,3 +64,22 @@ $me.mousemove (function (e) {//!!!!!!!
 $('.cal-body').mousedown (function (e) {
   e.preventDefault ()
 })
+
+
+
+// .on('touchstart mousedown', function(e) {
+//   e.preventDefault();
+//   someAction();
+// });
+
+
+// .on('touchend mouseup', function(e) {
+//   e.preventDefault();
+//   someAction();
+// });
+
+
+// .on('touchmove mousemove', function(e) {
+//   e.preventDefault();
+//   someAction();
+// });
--- start.js
+++ start.js
@@ -1,14 +1,18 @@
-var startYear = 1970;
-var endYear = 2030;
-var lang = "ru" 
+//preparation, set gefault values of year and month
 var forebrDate = new Date();
+var currentYear = forebrDate.getFullYear()
+var startYear = currentYear - 50;
+var endYear = currentYear + 20;
+var lang = "ru"
+
+
 
 setSelects(startYear, endYear);
 
 setCurrentDate();
 
 function setSelects(startYear, endYear) {
-	for (var i = startYear; i < endYear; i++) {
+	for (var i = startYear; i <= endYear; i++) {
 		let option = document.createElement("option");
 		option.textContent = i;
 		option.value = i;
@@ -19,7 +23,9 @@ function setSelects(startYear, endYear) {
 	for (var i = 0; i < 12; i++) {
 		let option = document.createElement("option");
 		d.setMonth(i);
-		let monthName =  d.toLocaleString(lang, { month: 'long'});
+		let monthName = d.toLocaleString(lang, {
+			month: 'long'
+		});
 		option.textContent = monthName;
 		option.value = i;
 		document.getElementById("forebrMonth").appendChild(option);
@@ -30,7 +36,7 @@ function setSelects(startYear, endYear) {
 
 function setCurrentDate() {
 	var d = new Date();
-	document.getElementById("forebrMonth").selectedIndex  = d.getMonth();
-	document.getElementById("forebrYear").selectedIndex  = d.getFullYear() - startYear;
-	
+	document.getElementById("forebrMonth").selectedIndex = d.getMonth();
+	document.getElementById("forebrYear").selectedIndex = d.getFullYear() - startYear;
+
 }
